1de355f07746247e2aece37240df0a0dbabe341b
jwatts82 <jessperr182@hotmail.com>
2020-10-08 10:53:52 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,18 +3,68 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM students"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-  end
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
+    
+    self.new_from_db(DB[:conn].execute(sql, name)[0])
   
+
+   # DB[:conn].execute(sql, name).map do |row|
+   #   self.new_from_db(row)
+   # end.first
+  end
+
+  def self.all_students_in_grade_9
+    sql = "SELECT * FROM students WHERE grade = 9"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT #{num}"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    row = DB[:conn].execute(sql)[0]
+    self.new_from_db(row)
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = #{grade}"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
+  end
+
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
